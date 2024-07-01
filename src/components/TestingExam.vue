e1f1581d00e71696c2283bb362315a004175e272
Mukesh Rawat <mukeshrawat68@gmail.com>
2024-07-01 16:58:03 +0300

    Update dependencies and add new packages, including "@heroicons/vue", "postcss", "tailwindcss", "@alloc/quick-lru", "any-promise", "anymatch", "binary-extensions", and "braces". Also update versions for existing packages.

---
+++ src/components/TestingExam.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import { items } from "./movies.json";
+import { StarIcon } from "@heroicons/vue/24/solid";
+
+</script>
+
+<template>
+    <div class="container mx-auto bg-teal-400">
+        this is test
+    </div>
+</template>
---
+++ src/views/TestView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TestingExam from '../components/TestingExam.vue'
+</script>
+
+<template>
+  <main>
+    <TestingExam />
+  </main>
+</template>
